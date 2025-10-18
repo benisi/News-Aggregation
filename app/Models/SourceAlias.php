63f3721c3a684d56c379c881fae34596fc8f0320
Benjamin Isidahomen <bisidahomen@gmail.com>
2025-10-18 13:00:27 +0100

    feat: add models to support aggregator

---
+++ app/Models/SourceAlias.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class SourceAlias extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'source_id',
+    ];
+
+    public function source(): BelongsTo
+    {
+        return $this->belongsTo(Source::class);
+    }
+}
