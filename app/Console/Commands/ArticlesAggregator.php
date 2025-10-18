ad2fa5d574d823f4fc7587d892bf8a9354fc5f0e
Benjamin Isidahomen <bisidahomen@gmail.com>
2025-10-18 13:51:51 +0100

    feat: add aggregator command

---
+++ app/Console/Commands/ArticlesAggregator.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Enums\DataSourceEnum;
+use App\Jobs\AggregateArticle;
+use Illuminate\Console\Command;
+
+class ArticlesAggregator extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'articles:aggregate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'poll articles from various data sources';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        foreach (DataSourceEnum::cases() as $source) {
+            $fetcher = new ($source->getFetcher());
+            dispatch(new AggregateArticle($fetcher));
+        }
+    }
+}
