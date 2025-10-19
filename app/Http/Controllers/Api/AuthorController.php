f033c09a40d41c99baa8e6e6badb8e2bd4337a08
Benjamin Isidahomen <bisidahomen@gmail.com>
2025-10-19 07:45:57 +0100

    feat: allow per page to author list route

--- app/Http/Controllers/Api/AuthorController.php
+++ app/Http/Controllers/Api/AuthorController.php
@@ -8,9 +8,19 @@ use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
+    const DEFAULT_PER_PAGE = 50;
+    const MAX_PER_PAGE = 100;
+
     public function index(Request $request, SearchAuthorsAction $searchAuthors)
     {
         $searchTerm = $request->query('search');
-        return $searchAuthors->execute($searchTerm);
+        $requestPerPage = (int) $request->query('per_page', self::DEFAULT_PER_PAGE);
+        $perPage = self::DEFAULT_PER_PAGE;
+
+        if ($requestPerPage > 0) {
+            $perPage = max(1, min(self::MAX_PER_PAGE, $requestPerPage));
+        }
+
+        return $searchAuthors->execute($searchTerm, $perPage);
     }
 }
