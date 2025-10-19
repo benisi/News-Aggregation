29a3dafadcba0d2f57cab988354eba15ca96bf17
Benjamin Isidahomen <bisidahomen@gmail.com>
2025-10-19 13:33:44 +0100

    feat: add auth feature

---
+++ app/DTOs/LoginDTO.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DTOs;
+
+use App\Http\Requests\LoginRequest;
+
+readonly class LoginDTO
+{
+    public function __construct(
+        public string $email,
+        public string $password,
+    ) {}
+
+    public static function fromRequest(LoginRequest $request): self
+    {
+        return new self(
+            email: $request->validated('email'),
+            password: $request->validated('password'),
+        );
+    }
+}
---
+++ app/DTOs/RegisterDTO.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DTOs;
+
+use App\Http\Requests\RegisterRequest;
+
+readonly class RegisterDTO
+{
+    public function __construct(
+        public string $name,
+        public string $email,
+        public string $password,
+    ) {}
+
+    public static function fromRequest(RegisterRequest $request): self
+    {
+        return new self(
+            name: $request->validated('name'),
+            email: $request->validated('email'),
+            password: $request->validated('password'),
+        );
+    }
+}
